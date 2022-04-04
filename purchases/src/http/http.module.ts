b68290d38a4a22fd46b5bfba147ff16f209d357c
alef <alef.moura@redfox.tech>
2022-04-04 11:16:47 -0300

    add projects

---
+++ purchases/src/http/http.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql';
+import { DatabaseModule } from '../database/database.module';
+import path from 'path'
+import { ApolloDriver } from '@nestjs/apollo';
+@Module({
+    imports: 
+    [ConfigModule.forRoot(),
+     DatabaseModule,
+    GraphQLModule.forRoot({
+        driver: ApolloDriver,
+        autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql')
+    })],
+    providers: []
+})
+export class HttpModule {}
